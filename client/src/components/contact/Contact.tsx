1dced57fa9818c16cbd6497927f44e1348979f98
josepabellana <josep.abe.pu@gmail.com>
2023-04-25 23:43:22 +0200

    x

--- client/src/components/contact/Contact.tsx
+++ client/src/components/contact/Contact.tsx
@@ -39,7 +39,7 @@ const Contact = () => {
               </a>
             </div>
 
-            <div className="contact__card">
+            {/* <div className="contact__card">
               <i className="bx bxl-whatsapp contract__card-icon"></i>
               <h3 className="contact__card-title">Whatsapp</h3>
               <span className="contact__card-data">+34 647 625 609</span>
@@ -47,7 +47,7 @@ const Contact = () => {
                 Write me
                 <i className="bx bx-right-arrow-alt contact__button-icon"></i>
               </a>
-            </div>
+            </div> */}
           </div>
         </div>
 
