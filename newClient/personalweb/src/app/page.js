daf18aaa73d9bb828161b80e5f1ca597c4356e9e
josepabellana <josep.abe.pu@gmail.com>
2025-02-10 21:14:02 +0100

    feat: new web
    nextjs new app

---
+++ newClient/personalweb/src/app/page.js
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/profile-BN-closer.png"
+          alt="Josep"
+          width={480}
+          height={408}
+          priority
+        />
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="/experience"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Experience
+        </a>
+        <a
+          href="/blog"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Blog
+        </a>
+        <a
+          href="/finance"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Finance
+        </a>
+      </footer>
+    </div>
+  );
+}
