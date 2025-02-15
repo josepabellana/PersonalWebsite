8367c0f3259780a6b629cd49bdbce147d7bbf819
josepabellana <josep.abe.pu@gmail.com>
2025-02-15 15:39:50 +0100

    feat: adding work in progress and rooting

---
+++ newClient/personalweb/src/app/experience/page.js
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Page() {
+    return (
+        <main className={styles.main}>
+            <p className={styles.disclaimer}>This page is still a work in progress.</p>
+        </main>
+    );
+}
--- newClient/personalweb/src/app/page.js
+++ newClient/personalweb/src/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -17,45 +18,36 @@ export default function Home() {
         />
       </main>
       <footer className={styles.footer}>
-        <a
-          href="/experience"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          What I do
-        </a>
-        <a
-          href="/blog"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Blog
-        </a>
-        <a
-          href="/finance"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Finance
-        </a>
+        <Link href="/experience" passHref>
+            <Image
+              aria-hidden
+              src="/file.svg"
+              alt="File icon"
+              width={16}
+              height={16}
+            />
+            What I do
+        </Link>
+        <Link href="/blog" passHref>
+            <Image
+              aria-hidden
+              src="/window.svg"
+              alt="Window icon"
+              width={16}
+              height={16}
+            />
+            Blog
+        </Link>
+        <Link href="/finance" passHref>
+            <Image
+              aria-hidden
+              src="/globe.svg"
+              alt="Globe icon"
+              width={16}
+              height={16}
+            />
+            Finance
+        </Link>
       </footer>
     </div>
   );
